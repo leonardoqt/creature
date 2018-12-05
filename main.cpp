158072111399072b66184461fcb2790fb4eb2345
Tian Qiu <tianq@sas.upenn.edu>
2018-12-05 11:26:51 -0500

    first evolution test

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
 	int num_organ = 90000;
 	int len_gene = 9;
 	int num_resource = 2;
-	int num_creature_opt = 100;
+	int num_creature_opt = 500;
 	int num_creature = num_creature_opt;
 	int num_creature_next;
 	// code
@@ -39,7 +39,7 @@ int main()
 	// for move
 	int move_max = 10000;
 	// for generation
-	int gen_max = 20;
+	int gen_max = 200;
 	// for load/save
 	int ini_from_file = 0;
 
@@ -100,6 +100,11 @@ int main()
 			being[t1].save_dna(out_dna);
 		}
 		being[t1].translate();
+	}
+	if (!ini_from_file)
+	{
+		out_dna.close();
+		out_dna.open("dna_last.dat");
 	}
 
 	// the trip of one generation
@@ -173,4 +178,7 @@ int main()
 			cout<<being[t1].x<<'\t'<<being[t1].y<<'\t'<<num_gen<<endl;
 		}
 	}
+	for(int t1=0; t1<num_creature; t1++)
+		being[t1].save_dna(out_dna);
+	return 0;
 }
---
+++ plot_gen.m
@@ -0,0 +1,18 @@
+in = dlmread('ss.dat');
+x = in(:,1);
+y = in(:,2);
+gen = in(:,3);
+frame=24;
+
+x_max = max(x);
+x_min = min(x);
+y_max = max(y);
+y_min = min(y);
+num = max(gen);
+
+for i=0:num
+	scatter(x(gen==i),y(gen==i));
+	xlim([x_min,x_max])
+	ylim([y_min,y_max])
+	pause(1/frame)
+end
